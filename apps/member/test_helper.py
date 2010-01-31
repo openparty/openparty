c1b281340142ccea2cebc8cad3b2a9661a78c53b
iamtin <iamtin@675f3c5f-904a-0410-84ce-8d0eee00fd03>
2010-01-31 09:16:17 +0000

    增加了测试helper，争取不使用fixture功能测试，使用数据库手工准备
    git-svn-id: https://openparty.googlecode.com/svn/trunk/openparty@23 675f3c5f-904a-0410-84ce-8d0eee00fd03

---
+++ apps/member/test_helper.py
@@ -0,0 +1,8 @@
+from openparty.apps.member.forms import SignupForm
+
+def create_user(email='tin@domain.com', password='123', nickname='tin', activate=True):
+	signup_form = SignupForm({ 'email': email, 'password1': password, 'nickname': nickname, 'password2': password })
+	member = signup_form.save()
+	if activate:
+		member.user.is_active = True
+		member.user.save()
