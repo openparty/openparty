0177e741f7d5d706a6c45400c28dd5be7413fd6d
iamtin <iamtin@675f3c5f-904a-0410-84ce-8d0eee00fd03>
2010-01-31 08:13:09 +0000

    重构，将forms.py拆成多个文件，方便快速定位
    git-svn-id: https://openparty.googlecode.com/svn/trunk/openparty@20 675f3c5f-904a-0410-84ce-8d0eee00fd03

---
+++ apps/member/forms/login_form.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from django.contrib.auth import authenticate, login
+
+
+class LoginForm(forms.Form):
+	email = forms.EmailField(label=u'email地址', required=True, widget=forms.TextInput())
+	password = forms.CharField(label=u'密码', required=True, widget=forms.PasswordInput(render_value=False))
+	remember = forms.BooleanField(label=u'记住登陆信息', help_text=u'如果选择记住登陆信息，会保留登陆信息2周', required = False)
+	
+	user = None
+	
+	def clean(self):
+		credential = { 'username': self.cleaned_data['email'], 'password': self.cleaned_data['password'] }
+		user = authenticate(**credential)
+		if user:
+			if user.is_active:
+				self.user = user
+			else:
+				raise forms.ValidationError(u'您还没有通过邮件激活帐号，请您登陆邮箱打开链接激活')
+		else:
+			raise forms.ValidationError(u'您输入的邮件地址与密码不匹配或者帐号还不存在，请您重试或者<a href="/signup">注册帐号</a>')
+		return self.cleaned_data
+
+	def login(self, request):
+		if self.is_valid():
+			login(request, self.user)
+			if self.cleaned_data["request"]:
+				request.session.set_expiry(60 * 60 * 24 * 7 * 3)
+			else:
+				request.session.set_expiry(0)
+			return True
+		return False
--- apps/member/forms/signup_form.py
+++ apps/member/forms/signup_form.py
@@ -2,37 +2,7 @@
 import re
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
 from openparty.apps.member.models import Member
-
-class LoginForm(forms.Form):
-	email = forms.EmailField(label=u'email地址', required=True, widget=forms.TextInput())
-	password = forms.CharField(label=u'密码', required=True, widget=forms.PasswordInput(render_value=False))
-	remember = forms.BooleanField(label=u'记住登陆信息', help_text=u'如果选择记住登陆信息，会保留登陆信息2周', required = False)
-	
-	user = None
-	
-	def clean(self):
-		credential = { 'username': self.cleaned_data['email'], 'password': self.cleaned_data['password'] }
-		user = authenticate(**credential)
-		if user:
-			if user.is_active:
-				self.user = user
-			else:
-				raise forms.ValidationError(u'您还没有通过邮件激活帐号，请您登陆邮箱打开链接激活')
-		else:
-			raise forms.ValidationError(u'您输入的邮件地址与密码不匹配或者帐号还不存在，请您重试或者<a href="/signup">注册帐号</a>')
-		return self.cleaned_data
-
-	def login(self, request):
-		if self.is_valid():
-			login(request, self.user)
-			if self.cleaned_data["remember"]:
-				request.session.set_expiry(60 * 60 * 24 * 7 * 3)
-			else:
-				request.session.set_expiry(0)
-			return True
-		return False
 
 class SignupForm(forms.Form):
 	email = forms.EmailField(label=u'email地址', required=True, widget=forms.TextInput())
@@ -50,7 +20,8 @@ class SignupForm(forms.Form):
 	def clean_email(self):
 		email = self.cleaned_data["email"].strip().lower()
 		try:
-			User.objects.get(username__iexact=email)
+			# 每个Member对应一个User，但是允许后台User不对应Member
+			User.objects.get(username__iexact=email) 
 		except User.DoesNotExist:
 			return email
 		raise forms.ValidationError(u'已经有用户使用“%s”注册了用户，请您确认邮件是否拼写错误' % email)
