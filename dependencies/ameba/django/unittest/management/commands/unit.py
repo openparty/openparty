99f94c888bb9703bd0d4fe90e6f16aa4e17476fa
iamtin <iamtin@675f3c5f-904a-0410-84ce-8d0eee00fd03>
2010-02-09 05:29:08 +0000

    增加了单纯的unittest command
    git-svn-id: https://openparty.googlecode.com/svn/trunk/openparty@34 675f3c5f-904a-0410-84ce-8d0eee00fd03

---
+++ dependencies/ameba/django/unittest/management/commands/unit.py
@@ -0,0 +1,37 @@
+import os
+import unittest
+ 
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if len(args) > 0:
+            self.test_case = args[0]
+        self.handle_noargs(**options)
+        
+    def handle_noargs(self, **options):
+        self.project_name = os.path.basename(settings.PROJECT_PATH)
+        self.unittests_path = os.path.join(settings.PROJECT_PATH, 'tests', 'unittests')
+        unittest.TextTestRunner(verbosity=1).run(self.get_suite())
+
+    def get_suite(self):
+        modules = []
+        for root, _, files in os.walk(self.unittests_path):
+            if not files: continue
+            for file in files:
+                if not (file.startswith('test_') and file.endswith('.py')): continue     
+                path = '%s.%s' % (root.replace(self.unittests_path, '').replace('/', '.'), file)
+                module = self.project_name + '.tests.unittests.' + path[1:].replace('.py', '')
+                
+                if hasattr(self, 'test_case'):
+                    if file == 'test_' + self.test_case + '.py':
+                        modules.append(__import__(module, fromlist=['*']))
+                        break
+                else:
+                    modules.append(__import__(module, fromlist=['*']))
+        
+        load = unittest.defaultTestLoader.loadTestsFromModule
+        return unittest.TestSuite(map(load, modules))
--- settings.py
+++ settings.py
@@ -6,17 +6,17 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-	# ('Your Name', 'your_email@domain.com'),
+    # ('Your Name', 'your_email@domain.com'),
 )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'			# 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'openparty'				# Or path to database file if using sqlite3.
-DATABASE_USER = 'root'			   # Not used with sqlite3.
-DATABASE_PASSWORD = ''		   # Not used with sqlite3.
-DATABASE_HOST = '127.0.0.1'				# Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''			   # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'mysql'   # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'openparty' # Or path to database file if using sqlite3.
+DATABASE_USER = 'root'      # Not used with sqlite3.
+DATABASE_PASSWORD = ''      # Not used with sqlite3.
+DATABASE_HOST = '127.0.0.1' # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''          # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -54,35 +54,35 @@ SECRET_KEY = ')x_^bgbj$v_r-5=rn&01pm-5*%szlnc+8^3kh4$^$#z-gn5yo#'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-	'django.template.loaders.filesystem.load_template_source',
-	'django.template.loaders.app_directories.load_template_source',
-#	  'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
 )
 
 MIDDLEWARE_CLASSES = (
-	'django.middleware.common.CommonMiddleware',
-	'django.contrib.sessions.middleware.SessionMiddleware',
-	'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
 ROOT_URLCONF = 'openparty.urls'
 
 TEMPLATE_DIRS = (
-	# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-	# Always use forward slashes, even on Windows.
-	# Don't forget to use absolute paths, not relative paths.
-	os.path.join(PROJECT_PATH, 'templates'),
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
     'django.contrib.admin',
-	'django.contrib.auth',
-	'django.contrib.contenttypes',
-	'django.contrib.sessions',
-	'django.contrib.sites',
-	
-	'openparty.apps.core',
-	'openparty.apps.member',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'dependencies.ameba.django.unitest'
+    'openparty.apps.core',
+    'openparty.apps.member',
 )
 
 INTERNAL_IPS = ('127.0.0.1')
@@ -90,6 +90,6 @@ INTERNAL_IPS = ('127.0.0.1')
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
 try:
-	from local_settings import *
+    from local_settings import *
 except ImportError:
-	pass
+    pass
