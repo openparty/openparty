e4ca7fd0474ac108282cc59c92633547640be748
cnborn <cnborn@675f3c5f-904a-0410-84ce-8d0eee00fd03>
2010-02-18 06:37:39 +0000

    Admin界面的调整
    git-svn-id: https://openparty.googlecode.com/svn/trunk/openparty@54 675f3c5f-904a-0410-84ce-8d0eee00fd03

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 from models import Event, Topic
-
-admin.site.register(Event)
-admin.site.register(Topic)
+class Event_Admin(admin.ModelAdmin):
+    list_display = ('name', 'datetime_begin')
+    list_filter = ['datetime_begin']
+admin.site.register(Event, Event_Admin)
+class Topic_Admin(admin.ModelAdmin):
+	list_display = ('name', 'author', 'total_votes', 'in_event')
+	list_filter = ['total_votes', 'author', 'in_event']
+admin.site.register(Topic, Topic_Admin)
 
 from models import Fav, Vote, Comment
 admin.site.register(Fav)
