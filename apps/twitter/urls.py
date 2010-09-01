a9f5362cea63a4e429ede1863f39cd892abd72b0
iamtin <iamtin@675f3c5f-904a-0410-84ce-8d0eee00fd03>
2010-09-01 15:33:59 +0000

    实现发推的功能，但是只限定在dev，需要验证在服务器环境是否可用
    git-svn-id: https://openparty.googlecode.com/svn/trunk/openparty@248 675f3c5f-904a-0410-84ce-8d0eee00fd03

--- apps/twitter/urls.py
+++ apps/twitter/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('twitter.views',
     url(r'^$', 'index', name='tweets'),
     url(r'^/request_oauth$', 'request_oauth', name='request_oauth'),
     url(r'^/oauth_callback$', 'oauth_callback', name='oauth_callback'),
+    url(r'^/update$', 'update', name='update_tweet'),
 )
--- apps/twitter/views.py
+++ apps/twitter/views.py
@@ -13,9 +13,14 @@ from apps.member.models import Member
 
 
 def index(request):
+    twitter_enabled = False
+    if hasattr(request, 'user'):
+        if request.user.username.lower() in ('iamtin@gmail.com', 'cnborn@gmail.com', 'makestory@gmail.com', 'cong.lin@gmail.com', 'greatcleverpig@gmail.com'):
+            twitter_enabled = True
     tweets = Tweet.objects.order_by('-tweet_id')[:100]
     ctx = {'tweets': tweets,
            'tab': 'tweet',
+           'twitter_enabled': twitter_enabled
           }
     return render_to_response('twitter/index.html', ctx, context_instance=RequestContext(request))
 
@@ -33,14 +38,35 @@ def request_oauth(request):
     if member.twitter_enabled:
         member.twitter_enabled = False
     member.save()
-
     return redirect(redirect_url, permanent=False)
 
 @login_required
 def oauth_callback(request):
     member = Member.objects.get(user=request.user)
-    member.twitter_enabled = True
-    member.save()
+    auth = tweepy.OAuthHandler(TWITTER_OPENPARTY_KEY, TWITTER_OPENPARTY_SECRET)
+    try:
+        auth.set_request_token(member.twitter_access_token_key, member.twitter_access_token_secret)
+        auth.get_access_token()
+        member.twitter_access_token_key = auth.access_token.key
+        member.twitter_access_token_secret = auth.access_token.secret
+        member.twitter_enabled = True
+        member.save()
+        messages.info(request, u'恭喜，您已经成功通过了Twitter的OAuth认证！以后您就可以在这里发推了。')
+    except tweepy.TweepError:
+        messages.error(request, u'对不起，认证的过程中发生了错误')
 
-    messages.info(request, u'恭喜，您已经成功通过了Twitter的OAuth认证！以后您就可以在这里发推了。')
+    return redirect(reverse('tweets'))
+
+@login_required
+def update(request):
+    member = Member.objects.get(user=request.user)
+    status = request.POST.get('status')
+    if member.twitter_enabled:
+        auth = tweepy.OAuthHandler(TWITTER_OPENPARTY_KEY, TWITTER_OPENPARTY_SECRET)
+        auth.set_access_token(member.twitter_access_token_key, member.twitter_access_token_secret)
+        api = tweepy.API(auth)
+        api.update_status(status)
+        messages.info(request, u'Sent!')
+    else:
+        messages.error(request, u'对不起您还没有通过Twitter的OAuth认证')
     return redirect(reverse('tweets'))
