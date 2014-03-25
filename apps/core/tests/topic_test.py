d616cdf8de02c7fb963f5d22755720a8cb150fc8
CNBorn <CNBorn@hotmail.com>
2014-03-25 19:00:04 +0800

    fix summary test

--- apps/core/tests/topic_test.py
+++ apps/core/tests/topic_test.py
@@ -11,7 +11,7 @@ class TopicTest(TestCase):
     def test_topic_summary(self):
         content = '''软件需求遇到的最大问题是什么？基本上都是沟通和交流的相关问题需求从哪里来：客户（市场）、用户 我们需要确定的是：谁是用户？当前业务流程情况？业务目标是什么？ 项目需求确定中遇到的最大问题是什么？需求文档驱动的过程不堪重负 查看更多'''
         t = Topic(content=content)
-        self.assertEquals(u'软件需求遇到的最大问题是什么？...', t.summary)
+        self.assertEquals(u'软件需求遇到的最大问题是什么？基本上都是沟通和交流的相关问题需求从哪里来：客户（市场）、用户 我们需要确定的是：谁是用户...', t.summary)
     
     def test_render_topic_content_restructuredtext(self):
         '''You have to install docutils for pass this test.'''
