4770fdee32cf0e773d12b431980f50571fbea523
Tin <iamtin@gmail.com>
2021-12-31 21:13:54 -0800

    hack: upgrade fabric; make the site run again with gunicorn;
    Sorry, it's ugly. fabric2's API seems silly, or maybe I'm silly.

--- fabfile.py
+++ fabfile.py
@@ -1,158 +1,176 @@
-from fabric.api import *
-
-# Default release is 'current'
-env.name = "openparty"
-env.release = "current"
-env.python_version = "2.7.2"
+from fabric import task
 
 
-@task
-def production():
-    """Production server settings"""
-    env.settings = "production"
-    env.user = "openparty"
-    env.path = "/home/%(user)s/sites/openparty-app" % env
-    env.hosts = ["www.beijing-open-party.com"]
+env = {
+    "user": "openparty",
+    "python_version": "3.9.9",
+    "release": "current",
+    "release_branch": "master",
+    "environment": "production",
+    "home_path": "/home/public_html/openparty-app",
+    "releases": "releases",
+}
+
+op_hosts = ["openparty@beijing-open-party.com"]
 
 
-@task
-def setup():
+def hostname(c):
+    c.run("echo running at $(hostname)")
+
+
+@task(hosts=op_hosts)
+def setup(c):
+    hostname(c)
     """
     Setup a fresh virtualenv and install everything we need so it's ready to deploy to
     """
-    with activate_virtualenv():
-        run("mkdir -p %(path)s; cd %(path)s; mkdir releases; mkdir shared;" % env)
-        clone_repo()
-        checkout_latest()
-        install_requirements()
+    activate_pyenv(c)
+    c.run("mkdir -p %(home_path)s; cd %(home_path)s; mkdir releases; mkdir shared;" % env)
+    clone_repo(c)
+    checkout_latest(c)
+    install_requirements(c)
 
 
-@task
-def deploy():
+# pipenv run fab deploy
+@task(hosts=op_hosts)
+def deploy(c):
     """Deploy the latest version of the site to the server and restart nginx"""
-    with activate_virtualenv():
-        checkout_latest()
-        install_requirements()
-        symlink_current_release()
-        migrate()
-        restart_server()
+    activate_pyenv(c)
+    checkout_latest(c)
+    install_requirements(c)
+    symlink_current_release(c)
+    migrate(c)
+    restart_server(c)
 
 
-def clone_repo():
+def clone_repo(c):
+    hostname(c)
     """Do initial clone of the git repo"""
-    run(
-        "cd %(path)s; git clone https://github.com/openparty/openparty.git repository"
-        % env
-    )
+    c.run("cd %(home_path)s; git clone https://github.com/openparty/openparty.git repository" % env)
 
 
-def checkout_latest():
+@task(hosts=op_hosts)
+def checkout_latest(c):
+    print("checkout_latest")
+    hostname(c)
     """Pull the latest code into the git repo and copy to a timestamped release directory"""
-    import time
 
-    env.release = time.strftime("%Y%m%d%H%M%S")
-    run("cd %(path)s/repository; git pull origin master" % env)
-    run(
-        "cp -R %(path)s/repository %(path)s/releases/%(release)s; rm -rf %(path)s/releases/%(release)s/.git*"
-        % env
-    )
+    with c.cd("%(home_path)s/repository" % env):
+        c.run("git checkout %(release_branch)s" % env)
+        c.run("git pull origin %(release_branch)s" % env)
+        env["release"] = c.run("git rev-parse HEAD").stdout.strip()
 
-
-def install_requirements():
-    """Install the required packages using pip"""
-    """Pip need copy file into system folder, so we need a sudo"""
-    run("cd %(path)s; pip install -r ./releases/%(release)s/requirements.txt" % env)
+    if c.run("ls %(home_path)s/releases/%(release)s" % env, warn=True):
+        print("release folder already exists, skip the checkout-release step.")
+    else:
+        c.run("cp -R %(home_path)s/repository %(home_path)s/releases/%(release)s;" % env)
+        c.run("rm -rf %(home_path)s/releases/%(release)s/.git*" % env)
 
 
-def symlink_current_release():
+def install_requirements(c):
+    print("install_requirements")
+    with c.cd("%(home_path)s/releases/%(release)s" % env):
+        c.run("PIPENV_VENV_IN_PROJECT=enabled pipenv install" % env)
+
+
+def symlink_current_release(c):
+    print("symlink_current_release")
     """Symlink our current release, uploads and settings file"""
-    with settings(warn_only=True):
-        run(
-            "cd %(path)s; rm releases/previous; mv releases/current releases/previous;"
-            % env
-        )
-    run("cd %(path)s; ln -s %(release)s releases/current" % env)
-    """ production settings"""
-    run(
-        "cd %(path)s/releases/current/; cp %(path)s/conf/local_settings.py local_settings.py"
-        % env
-    )
-    run(
-        "cd %(path)s/releases/current/; cp %(path)s/conf/site-restart site-restart"
-        % env
-    )
-    with settings(warn_only=True):
-        run(
-            "cd %(path)s/releases/current/media; ln -s %(path)s/shared/post_images post_images"
-            % env
-        )
-        run(
-            "cd %(path)s/releases/current/media; rm -rf upload; ln -s %(path)s/shared/upload upload"
-            % env
-        )
-        run(
-            "cd %(path)s/releases/current/media; ln -s /usr/local/lib/python2.7/dist-packages/django/contrib/admin/static/admin ."
-            % env
-        )
-        # run('rm %(path)s/shared/static' % env)
-        # run('cd %(path)s/releases/current/static/; ln -s %(path)s/releases/%(release)s/static %(path)s/shared/static ' %env)
+    # if c.run("ls %(home_path)s/releases/previous" % env, warn=True):
+    #     c.run("rm -rf %(home_path)s/releases/previous" % env, warn=True)
+    # if c.run("ls %(home_path)s/releases/current" % env, warn=True):
+    #     c.run("mv %(home_path)s/releases/current %(home_path)s/releases/previous" % env, warn=True)
+    print("ln -s %(home_path)s/releases/%(release)s %(home_path)s/releases/current" % env)
+    c.run("ln -s %(home_path)s/releases/%(release)s %(home_path)s/releases/current" % env, warn=True)
+
+    c.run("cp %(home_path)s/conf/local_settings.py %(home_path)s/releases/current/local_settings.py" % env, warn=True)
+    c.run("cp %(home_path)s/conf/site-restart %(home_path)s/releases/current/site-restart" % env, warn=True)
+
+    c.run("ln -s %(home_path)s/shared/post_images %(home_path)s/releases/current/media/post_images" % env, warn=True)
+    c.run("rm -rf %(home_path)s/releases/current/media/upload" % env, warn=True)
+    c.run("ln -s %(home_path)s/shared/upload %(home_path)s/releases/current/media/upload" % env, warn=True)
+    # c.run(
+    #     "cd %(home_path)s/releases/current/media; ln -s /usr/local/lib/python2.7/dist-packages/django/contrib/admin/static/admin ."
+    #     % env
+    # )
+    # run('rm %(home_path)s/shared/static' % env)
+    # run('cd %(home_path)s/releases/current/static/; ln -s %(home_path)s/releases/%(release)s/static %(home_path)s/shared/static ' %env)
 
 
-def migrate():
+def migrate(c):
+    print("migrate")
     """Run our migrations"""
-    run(
-        "cd %(path)s/releases/current; python manage.py syncdb --noinput --migrate"
-        % env
-    )
-    run("cd %(path)s/releases/current; python manage.py migrate --noinput core" % env)
+    # I need to fix the south -> django database change first
+    # with c.cd("%(home_path)s/releases/%(release)s" % env):
+    #     c.run("pipenv run python manage.py migrate --noinput" % env)
 
 
-def rollback():
-    """
-    Limited rollback capability. Simple loads the previously current
-    version of the code. Rolling back again will swap between the two.
-    """
-    run("cd %(path)s; mv releases/current releases/_previous;" % env)
-    run("cd %(path)s; mv releases/previous releases/current;" % env)
-    run("cd %(path)s; mv releases/_previous releases/previous;" % env)
-    restart_server()
+# def rollback(c):
+#     print("rollback")
+#     """
+#     Limited rollback capability. Simple loads the previously current
+#     version of the code. Rolling back again will swap between the two.
+#     """
+#     c.run("cd %(home_path)s; mv releases/current releases/_previous;" % env)
+#     c.run("cd %(home_path)s; mv releases/previous releases/current;" % env)
+#     c.run("cd %(home_path)s; mv releases/_previous releases/previous;" % env)
+#     restart_server()
 
 
-@task
-def restart_server():
+def kill_current_cgi_process(c):
+    print("kill_current_cgi_process")
+    c.run("kill -9 `ps aux|grep python|grep gunicorn|awk '{print $2}'`", warn=True)
+
+
+def start_cgi_process(c):
+    print("start_cgi_process")
+    with c.cd("%(home_path)s/%(releases)s/%(release)s" % env):
+        c.run("PIPENV_VENV_IN_PROJECT=enabled pipenv run gunicorn -c gunicorn_config.py" % env)
+
+
+@task(hosts=op_hosts)
+def restart_server(c):
+    print("restart_server")
     """Restart the web server"""
-    with activate_virtualenv():
-        run(
-            "cd %(path)s/releases/current; %(path)s/releases/current/site-restart" % env
-        )
-        sudo("/etc/init.d/nginx restart")
+    activate_pyenv(c)
+    prepare_gunicorn(c)
+    kill_current_cgi_process(c)
+    start_cgi_process(c)
+    c.sudo("service nginx restart")
 
 
-def activate_virtualenv():
-    return prefix(
-        "PATH=/home/%(user)s/.pythonbrew/bin:$PATH source /home/%(user)s/.pythonbrew/etc/bashrc "
-        "&& pythonbrew use %(python_version)s && pythonbrew venv use %(name)s-%(settings)s"
+def prepare_gunicorn(c):
+    c.sudo("mkdir -p /var/{log,run}/gunicorn")
+    c.sudo("chown openparty:openparty /var/{log,run}/gunicorn")
+
+
+def activate_pyenv(c):
+    c.run(
+        "export PATH=%(home_path)s/.pyenv/shims:%(home_path)s/.pyenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
         % env
     )
 
 
-@task
-def setup_pythonbrew():
-    with prefix(
-        "PATH=/home/%(user)s/.pythonbrew/bin:$PATH source /home/%(user)s/.pythonbrew/etc/bashrc"
-        % env
-    ):
-        run("pythonbrew install %(python_version)s" % env)
-        run("pythonbrew switch %(python_version)s" % env)
-        run("pythonbrew symlink")
-        run("pythonbrew venv init")
+@task(hosts=op_hosts)
+def setup_python(c):
+    print("setup_python")
+    c.run("rm -rf %(home_path)s/.pyenv || true" % env)
+    c.run("curl https://pyenv.run | bash")
+    # this only affect interactive shell
+    c.run("echo 'export PYENV_ROOT=\"$HOME/.pyenv\"' >> ~/.bashrc")
+    c.run("echo 'export PATH=\"$PYENV_ROOT/bin:$PATH\"' >> ~/.bashrc")
+    c.run("echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval \"$(pyenv init -)\"\nfi' >> ~/.bashrc")
+    activate_pyenv(c)
+    c.run("pyenv install %(python_version)s" % env)
+    c.run("pyenv global %(python_version)s" % env)
 
 
-@task
-def create_venv():
-    with prefix(
-        "PATH=/home/%(user)s/.pythonbrew/bin:$PATH source /home/%(user)s/.pythonbrew/etc/bashrc && pythonbrew use %(python_version)s"
-        % env
-    ):
-        run("pythonbrew venv create %(name)s-%(settings)s" % env)
-        run("pythonbrew venv use %(name)s-%(settings)s" % env)
+@task(hosts=op_hosts)
+def setup_venv(c):
+    print("setup_venv")
+    activate_pyenv(c)
+    c.run("pip install --upgrade pip")
+    c.run("pip install pipenv setuptools")
+    with c.cd("%(home_path)s/%(releases)s/%(release)s" % env):
+        c.run("PIPENV_VENV_IN_PROJECT=enabled pipenv --python %(python_version)s" % env)
+        c.run("PIPENV_VENV_IN_PROJECT=enabled pipenv install")
---
+++ gunicorn_config.py
@@ -0,0 +1,21 @@
+"""Gunicorn *development* config file"""
+
+# Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
+wsgi_app = "apps.wsgi:application"
+# The granularity of Error log outputs
+loglevel = "warn"
+# The number of worker processes for handling requests
+workers = 5
+# The socket to bind
+bind = "0.0.0.0:8888"
+# Restart workers when code changes (development only!)
+reload = False
+# Write access and error info to /var/log
+accesslog = errorlog = "/var/log/gunicorn/openpartyapp.log"
+# Redirect stdout/stderr to log file
+capture_output = True
+# PID file so you can easily fetch process ID
+pidfile = "/var/run/gunicorn/openpartyapp.pid"
+# Daemonize the Gunicorn process (detach & enter background)
+daemon = True
+
